8fc921d73f53498bc5e64b5f6efafaa580a58071
yellows8 <yellows8@users.noreply.github.com>
2014-11-23 19:44:41 -0500

    Updated README and Makefile. Added v1.0.4 support. Fixed broken USA demo support.

--- smashbroshax.h
+++ smashbroshax.h
@@ -10,10 +10,20 @@
 #define POP_R0R4SLIPPC 0x001ca5b4 //pop {r0, r1, r2, r3, r4, sl, ip, pc}
 #define POP_R0PC 0x001bb47c //"pop {r0, pc}"
 
+#if REGION==2//EUR
 #define ROP_LDRR1R1_STRR1R0 0x0066e23c //"ldr r1, [r1]" "str r1, [r0]" "bx lr"
 #define ROP_MOVR1R3_BXIP 0x00769aa0 //"mov r1, r3" "bx ip"
 #define ROP_LDRR2R0_SUBR1R2R1_STRR1R0 0x00602404 //"ldr r2, [r0]" "sub r1, r2, r1" "str r1, [r0]" "bx lr"
 #define ROP_LDRR1R5_MOVR0R8_BLXR7 0x00b158e4 //"ldr r1, [r5]" "mov r0, r8" "blx r7"
+#elif REGION==1//USA
+
+#define ROP_LDRR1R1_STRR1R0 0x0066e264
+#define ROP_MOVR1R3_BXIP 0x00769ac8
+#define ROP_LDRR2R0_SUBR1R2R1_STRR1R0 0x0060242c
+#define ROP_LDRR1R5_MOVR0R8_BLXR7 0x00b1590c
+#else
+#error "This appbuild+region isn't(?) fully supported."
+#endif
 
 #define MEMCPY 0x0016e350
 
@@ -29,10 +39,6 @@
 #define GSPGPU_FLUSHDCACHE 0x0015cea4
 
 #define GXLOW_CMD4 0x00171cc8
-
-#define TEXT_APPMEM_OFFSET 0x00b00000 //Offset for physical-mem .text, relative to application mem-region end.
-
-#define ADDITONALDATA_SIZE1 0x18A0
 
 #elif APPBUILD==100//Full-game v1.0.0.
 
@@ -69,10 +75,6 @@
 
 #endif
 
-#define TEXT_APPMEM_OFFSET 0x00a00000
-
-#define ADDITONALDATA_SIZE1 0x2092
-
 #elif APPBUILD==102//Full-game v1.0.2.
 
 #define ADDITIONALDATA_ADR 0x00be57e8
@@ -101,11 +103,42 @@
 
 #define GXLOW_CMD4 0x00178af4
 
-#define TEXT_APPMEM_OFFSET 0x00a00000
+#elif APPBUILD==104//Full-game v1.0.4.
 
-#define ADDITONALDATA_SIZE1 0x2092
+#if REGION==3//JPN
+#error "This appbuild+region isn't fully supported."
+#else
+
+#define ADDITIONALDATA_ADR 0x00be97e8
+#define STACKPIVOT_ADR 0x0012df38
+#define POP_PC 0x0010dbfc
+#define POP_R0R4SLIPPC 0x001d67b4
+#define POP_R0PC 0x001c765c
+
+#define ROP_LDRR1R1_STRR1R0 0x003542dc
+#define ROP_MOVR1R3_BXIP 0x0057f46c
+#define ROP_LDRR2R0_SUBR1R2R1_STRR1R0 0x005e32a8
+#define ROP_LDRR1R5_MOVR0R8_BLXR7 0x00a7c6bc
+
+#define MEMCPY 0x0017514c
+
+#define SVCSLEEPTHREAD 0x001b51cc
+
+#define SRV_GETSERVICEHANDLE 0x001496a0
+
+#define NWMUDS_RecvBeaconBroadcastData 0x0035e228
+
+#define LOCALWLAN_SHUTDOWN 0x0035f284
+
+#define GSPGPU_SERVHANDLEADR 0x00b614a8
+#define GSPGPU_FLUSHDCACHE 0x0016300c
+
+#define GXLOW_CMD4 0x00178af4
+
+#endif
 
 #else
+
 #error "The specified APPBUILD value is not supported."
 #endif
 
@@ -113,10 +146,20 @@
 #define MOVSPLR_POPLRPC STACKPIVOT_ADR+0x14 //"mov sp, lr" "pop {lr, pc}"
 
 #if APPBUILD==0//demo
-#define TMPBUF_ADR 0x33F50000
+//#define TMPBUF_ADR 0x33F50000
+
+#define TEXT_APPMEM_OFFSET 0x00b00000 //Offset for physical-mem .text, relative to application mem-region end.
+
+#define ADDITONALDATA_SIZE1 0x18A0
 #else//full-game
-#define TMPBUF_ADR 0x33E18000//Full-game linearmem heap is smaller than the demo.
+//#define TMPBUF_ADR 0x33E18000//Full-game linearmem heap is smaller than the demo.
+
+#define TEXT_APPMEM_OFFSET 0x00a00000
+
+#define ADDITONALDATA_SIZE1 0x2092
 #endif
+
+#define TMPBUF_ADR 0x33B50000
 
 #define GXLOWCMD4_DSTADR_PTR TMPBUF_ADR+0x6000 //The value stored here is the dst-addr for gxlowcmd4.
 #define BEACONDATA_ADR TMPBUF_ADR+0x4000 //ADDITIONALDATA_ADR+0xb8
